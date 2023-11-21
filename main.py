d7b29076d9cd1bf38e3c5edf086a5b8de8ab74b4
darrenshun <l2.xu@vu.nl>
2023-11-21 17:30:10 +0100

    Refactored UI styling and layout in the PomodoroTimer class.

--- main.py
+++ main.py
@@ -1,26 +1,39 @@
 
 import tkinter as tk
 from tkinter import messagebox, simpledialog
+    
+# Styling constants
+BG_COLOR = "#282c34"
+FG_COLOR = "#abb2bf"
+BTN_BG = "#61afef"
+BTN_FG = "#282c34"
+FONT_NAME = "Courier New"
+TIMER_FONT = (FONT_NAME, 48, "bold")
+LABEL_FONT = (FONT_NAME, 14)
+BTN_FONT = (FONT_NAME, 14, "bold")
 
 class PomodoroTimer:
     def __init__(self, master):
         self.master = master
         self.master.title("Pomodoro Timer")
-        
-        self.timer_label = tk.Label(self.master, text="25:00", font=("Arial", 24))
-        # Improved UI layout with additional settings
-        self.timer_label.grid(row=0, column=0, columnspan=2, pady=20)
-        
-        self.start_button = tk.Button(self.master, text="Start", command=self.start_timer)
-        self.start_button.grid(row=1, column=0, padx=5, pady=10)
-        
-        self.stop_button = tk.Button(self.master, text="Stop", command=self.stop_timer, state=tk.DISABLED)
-        self.stop_button.grid(row=1, column=1, padx=5, pady=10)
-        
-        self.settings_button = tk.Button(self.master, text="Settings", command=self.set_timer)
-        self.settings_button.grid(row=2, column=0, columnspan=2, pady=10)
-        
-        # Configuration constants
+        self.master.configure(bg=BG_COLOR)
+
+        # Timer label with modern font and larger size
+        self.timer_label = tk.Label(self.master, text="25:00", font=TIMER_FONT, bg=BG_COLOR, fg=FG_COLOR)
+        self.timer_label.grid(row=0, column=0, columnspan=2, pady=(20, 10))
+
+        # Start button with vibrant color
+        self.start_button = tk.Button(self.master, text="Start", command=self.start_timer, font=BTN_FONT, bg=BTN_BG, fg=BTN_FG)
+        self.start_button.grid(row=1, column=0, padx=5, pady=10, ipadx=20)
+
+        # Stop button with vibrant color
+        self.stop_button = tk.Button(self.master, text="Stop", command=self.stop_timer, state=tk.DISABLED, font=BTN_FONT, bg=BTN_BG, fg=BTN_FG)
+        self.stop_button.grid(row=1, column=1, padx=5, pady=10, ipadx=20)
+
+        # Settings button with a modern look
+        self.settings_button = tk.Button(self.master, text="Settings", command=self.set_timer, font=LABEL_FONT, bg=BTN_BG, fg=BTN_FG)
+        self.settings_button.grid(row=2, column=0, columnspan=2, pady=(10, 20), ipadx=10)
+
         self.POMODORO_TIME = 1500  # 25 minutes in seconds
         self.BREAK_TIME = 300      # 5 minutes in seconds
         self.time_remaining = self.POMODORO_TIME
@@ -80,35 +93,8 @@ class PomodoroTimer:
             except Exception as e:
                 messagebox.showerror("Error", f"An error occurred: {e}")
     
+
 if __name__ == "__main__":
     root = tk.Tk()
     app = PomodoroTimer(root)
     root.mainloop()
-        # Styling constants
-        BG_COLOR = "#282c34"
-        FG_COLOR = "#abb2bf"
-        BTN_BG = "#61afef"
-        BTN_FG = "#282c34"
-        FONT_NAME = "Courier New"
-        TIMER_FONT = (FONT_NAME, 48, "bold")
-        LABEL_FONT = (FONT_NAME, 14)
-        BTN_FONT = (FONT_NAME, 14, "bold")
-
-        # Main window styling
-        self.master.configure(bg=BG_COLOR)
-
-        # Timer label with modern font and larger size
-        self.timer_label = tk.Label(self.master, text="25:00", font=TIMER_FONT, bg=BG_COLOR, fg=FG_COLOR)
-        self.timer_label.grid(row=0, column=0, columnspan=2, pady=(20, 10))
-
-        # Start button with vibrant color
-        self.start_button = tk.Button(self.master, text="Start", command=self.start_timer, font=BTN_FONT, bg=BTN_BG, fg=BTN_FG)
-        self.start_button.grid(row=1, column=0, padx=5, pady=10, ipadx=20)
-
-        # Stop button with vibrant color
-        self.stop_button = tk.Button(self.master, text="Stop", command=self.stop_timer, state=tk.DISABLED, font=BTN_FONT, bg=BTN_BG, fg=BTN_FG)
-        self.stop_button.grid(row=1, column=1, padx=5, pady=10, ipadx=20)
-
-        # Settings button with a modern look
-        self.settings_button = tk.Button(self.master, text="Settings", command=self.set_timer, font=LABEL_FONT, bg=BTN_BG, fg=BTN_FG)
-        self.settings_button.grid(row=2, column=0, columnspan=2, pady=(10, 20), ipadx=10)
